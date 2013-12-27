a16e8e7d062274100df1906d495fc6c0bac7d0ee
Adam Waxman <awaxman11@gmail.com>
2013-12-27 18:23:41 -0500

    Refactor

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
   
   $('.animation-1 .image').on('click', function(){
-    $(this).toggleClass('toggle-on');
-    $(this).toggleClass('toggle-animation-1');
+    $(this).toggleClass('toggle-animation');
   })
 
   $('.animation-2 .image').on('click', function(){
-    $(this).toggleClass('toggle-on');
-    $(this).toggleClass('toggle-animation-2');
+    $(this).toggleClass('toggle-animation-slow');
   })
 
   $('.animation-3 .image').on('click', function(){
     $(this).toggleClass('toggle-loading');
   })
+  
 })
